2ccdad0fb49fdcc9b1242981db3df6aafbc08f31
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-06 18:28:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,8 +13,8 @@ class App < Sinatra::Base
 
   get '/friends' do
     @friends = ['Emily Wilding Davison', 'Harrier Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
-
     erb :friends
+    
   end
 
 end
